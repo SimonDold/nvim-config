7f0bc44d5c5ac1b728010b71de708ffb4d5a2106
Simon Dold <simon.dold@stud.unibas.ch>
2024-06-29 23:00:11 +0200

    add shortcut for add and delete file.

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -12,6 +12,18 @@ require('nvim-tree').setup({
 
     vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Parent Node'))
     vim.keymap.set('n', 'l', api.node.open.edit, opts('Open Node'))
+    vim.keymap.set('n', 'd', function()
+        local node = api.tree.get_node_under_cursor()
+        api.fs.remove(node)
+    end, opts('Remove Node'))
+    vim.keymap.set('n', 'a', function ()
+       local node = api.tree.get_node_under_cursor()
+       if node and node.type == "directory" then
+           api.fs.create(node)
+       else
+           api.fs.create(node.parent)
+       end
+    end, opts('Add Node'))
   end
 })
 
